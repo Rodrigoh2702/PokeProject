95ab7c08379ff7a9a92e0c216e6f81a0425621b9
Rodrigoh2702 <rodrigoh.gongora@gmail.com>
2022-05-02 21:54:12 -0500

    Made changes to the modal

--- force-app/main/default/lwc/pokemonGrid/pokemonGrid.js
+++ force-app/main/default/lwc/pokemonGrid/pokemonGrid.js
@@ -1,11 +1,28 @@
 import { LightningElement, wire, track } from 'lwc';
 import getAllPokemons from '@salesforce/apex/pokemonController.getAllPokemons';
+import fetchMoreInfo from '@salesforce/apex/pokemonController.fetchMoreInfo';
 
 export default class PokemonGrid extends LightningElement {
     totalPokemons;
     visiblePokemons;
 
     @track showModal = false;
+
+    pokemonName;
+    pokemonAttack;
+    pokemonDefense;
+    pokemonHP;
+    pokemonSpAtk;
+    pokemonSpDef;
+    pokemonSpeed;
+    pokemonWeight;
+    pokemonHeight;
+    pokemonTypes;
+    pokemonImage;
+    pokemonNumber;
+    pokemonBaseExp;
+    pokemonAbilities;
+    pokemonImageLink;
     
     @wire(getAllPokemons)
     wiredPokemons({ error, data }) {
@@ -25,7 +42,28 @@ export default class PokemonGrid extends LightningElement {
 
     openModal(event) {
         this.showModal = true;
-        console.log(event.detail);
+        fetchMoreInfo({ pokedexNumber: event.currentTarget.dataset.id })
+        .then((result) => {
+            console.log(result);
+            this.pokemonName = result.Name;
+            this.pokemonAttack = result.Attack__c;
+            this.pokemonDefense = result.Defense__c;
+            this.pokemonHP = result.HP__c;
+            this.pokemonSpAtk = result.Special_Attack__c;
+            this.pokemonSpDef = result.Special_Defense__c;
+            this.pokemonSpeed = result.Speed__c;
+            this.pokemonWeight = result.Weight__c;
+            this.pokemonHeight = result.Height__c;
+            this.pokemonTypes = result.Types__c;
+            this.pokemonImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + result.Pokedex_Number__c +'.png';
+            //console.log(this.pokemonImage);
+            this.pokemonNumber = result.Pokedex_Number__c;
+            this.pokemonBaseExp = result.Base_Experience__c;
+            this.pokemonAbilities = result.Abilities__c;
+            
+        }).catch(error => {
+            console.log('Error: ', error);
+        });
     }
 
     closeModal(event) {
